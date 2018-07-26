122f42ea227e26178a55ae5014f20013dad6028f
Harald Klimach <harald.klimach@uni-siegen.de>
2018-07-26 21:06:18 +0200

    Added waf 2.0.9 sources and updated the waf file.

---
+++ build_waf.sh
@@ -0,0 +1,7 @@
+set -x
+UTESTS=$PWD/tools/utest_results.py
+SEQF=$PWD/tools/command_sequence.py
+bindir=$PWD
+
+TOOLS=c_bgxlc,c_nec,doxygen,eclipse,fc_nag,fc_nec,fc_bgxlf,fc_cray,fc_open64,fc_pgfortran,fc_solstudio,fc_xlf,print_commands,$UTESTS,$SEQF
+cd external/waf-2.0.9 && ./waf-light --make-waf --prelude='' --tools=$TOOLS && cp waf $bindir && cd -
