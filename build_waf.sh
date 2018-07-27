b6407bc18cc57b30b3e7d282b2722c743aacc7d6
Harald Klimach <harald.klimach@uni-siegen.de>
2018-07-27 10:20:37 +0200

    Go back to waf-1.9.15. In waf 2 the Fortran processing appears to be broken.

--- build_waf.sh
+++ build_waf.sh
@@ -4,4 +4,4 @@ SEQF=$PWD/tools/command_sequence.py
 bindir=$PWD
 
 TOOLS=c_bgxlc,c_nec,doxygen,eclipse,fc_nag,fc_nec,fc_bgxlf,fc_cray,fc_open64,fc_pgfortran,fc_solstudio,fc_xlf,print_commands,$UTESTS,$SEQF
-cd external/waf-2.0.9 && ./waf-light --make-waf --prelude='' --tools=$TOOLS && cp waf $bindir && cd -
+cd external/waf-1.9.15 && ./waf-light --make-waf --prelude='' --tools=$TOOLS && cp waf $bindir && cd -
