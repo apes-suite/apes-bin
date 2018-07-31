12f235eeeac595db4e645c9aad3fb4b395006725
Harald Klimach <harald.klimach@uni-siegen.de>
2018-07-31 20:07:13 +0200

    Added the option to use online or local external project information for FORD.

--- tools/make_fordoc.py
+++ tools/make_fordoc.py
@@ -1,3 +1,13 @@
+def options(opt):
+    opt.add_option('--fordonline', action='store_true', default=False,
+                   help='Use links to online documentation to refer to external projects in FORD')
+
+def configure(conf):
+    conf.env.fordonline = conf.options.fordonline
+
+def build(bld):
+    bld.env.fordonline = bld.env.fordonline or bld.options.fordonline
+
 def gendoc(task):
     """ A Task rule to generate Documentation with FORD
 
@@ -7,14 +17,19 @@ def gendoc(task):
         be the first target argument.
         All unique parent nodes of the files given as sources will be
         used as src_dir for the FORD command.
-        Pathes to external projects can be provided to the generator via
-        the 'extern' argument.
+        Pathes to external projects in the local file system can be
+        provided to the generator via the 'extern' argument.
+        URLs of external projects that are available online can be
+        provided via the the 'extern_urls' arguments.
     """
     import os
     tgt_path = os.path.dirname(task.outputs[0].abspath())
     src_paths = set()
+    use_urls = task.env.fordonline
     if not hasattr(task.generator, 'extern'):
         task.generator.extern = []
+    if not hasattr(task.generator, 'extern_urls'):
+        task.generator.extern_urls = []
     for srcfile in task.inputs:
         if srcfile not in task.generator.extern:
             src_paths.add(srcfile.parent.abspath())
@@ -25,8 +40,13 @@ def gendoc(task):
     for spath in src_paths:
         cmd.append('-d')
         cmd.append(spath)
-    for elink in task.generator.extern:
-        cmd.append('-L')
-        cmd.append(elink.parent.abspath())
+    if task.env.fordonline:
+        for elink in task.generator.extern_urls:
+            cmd.append('-L')
+            cmd.append(elink)
+    else:
+        for elink in task.generator.extern:
+            cmd.append('-L')
+            cmd.append(elink.parent.abspath())
     cmd.append(task.generator.mainpage)
     return task.exec_command( cmd, shell=False, cwd=task.generator.bld.top_dir )
