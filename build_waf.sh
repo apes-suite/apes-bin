47b0e146daeaf102ba662586b7b873658a7ce0f7
Harald Klimach <harald.klimach@uni-siegen.de>
2018-07-31 16:16:32 +0200

    Updated waf to 2.0.10, which contains a fix for the processing of generated Fortran files. Include a module for the processing of FORD.

--- build_waf.sh
+++ build_waf.sh
@@ -1,7 +1,8 @@
 set -x
 UTESTS=$PWD/tools/utest_results.py
 SEQF=$PWD/tools/command_sequence.py
+FORD=$PWD/tools/make_fordoc.py
 bindir=$PWD
 
-TOOLS=c_bgxlc,c_nec,doxygen,eclipse,fc_nag,fc_nec,fc_bgxlf,fc_cray,fc_open64,fc_pgfortran,fc_solstudio,fc_xlf,print_commands,$UTESTS,$SEQF
-cd external/waf-1.9.15 && ./waf-light --make-waf --prelude='' --tools=$TOOLS && cp waf $bindir && cd -
+TOOLS=c_bgxlc,c_nec,doxygen,eclipse,fc_nag,fc_nec,fc_bgxlf,fc_cray,fc_open64,fc_pgfortran,fc_solstudio,fc_xlf,print_commands,$UTESTS,$SEQF,$FORD
+cd external/waf-2.0.10 && ./waf-light --make-waf --prelude='' --tools=$TOOLS && cp waf $bindir && cd -
