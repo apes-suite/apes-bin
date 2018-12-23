c3bc51e87e0da7ffaba71e64d4837531da49cfed
Harald Klimach <harald.klimach@uni-siegen.de>
2018-12-23 14:07:53 +0100

    Updated to waf 2.0.14 to include support for submodules and NEC nfort compiler.

--- build_waf.sh
+++ build_waf.sh
@@ -4,5 +4,5 @@ SEQF=$PWD/tools/command_sequence.py
 FORD=$PWD/tools/make_fordoc.py
 bindir=$PWD
 
-TOOLS=c_bgxlc,c_nec,doxygen,eclipse,fc_nag,fc_nec,fc_bgxlf,fc_cray,fc_open64,fc_pgfortran,fc_solstudio,fc_xlf,print_commands,$UTESTS,$SEQF,$FORD
-cd external/waf-2.0.10 && ./waf-light --make-waf --prelude='' --tools=$TOOLS && cp waf $bindir && cd -
+TOOLS=c_bgxlc,c_nec,doxygen,eclipse,fc_nag,fc_nec,fc_nfort,fc_bgxlf,fc_cray,fc_open64,fc_pgfortran,fc_solstudio,fc_xlf,print_commands,$UTESTS,$SEQF,$FORD
+cd external/waf-2.0.14 && ./waf-light --make-waf --prelude='' --tools=$TOOLS && cp waf $bindir && cd -
