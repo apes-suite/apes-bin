f8c7f07410d6f61fe518292abc365f95d8045e63
Harald Klimach <harald.klimach@dlr.de>
2023-11-30 14:12:58 +0100

    Change revision module to use git instead of hg.

--- build_waf.sh
+++ build_waf.sh
@@ -5,4 +5,4 @@ FORD=$PWD/tools/make_fordoc.py
 bindir=$PWD
 
 TOOLS=clang_compilation_database,c_bgxlc,c_nec,doxygen,eclipse,fc_nag,fc_nec,fc_nfort,fc_bgxlf,fc_cray,fc_open64,fc_pgfortran,fc_solstudio,fc_xlf,print_commands,$UTESTS,$SEQF,$FORD
-cd external/waf-2.0.14 && ./waf-light --make-waf --prelude='' --tools=$TOOLS && cp waf $bindir && cd -
+cd external/waf-2.0.26 && ./waf-light --make-waf --prelude='' --tools=$TOOLS && cp waf $bindir && cd -
