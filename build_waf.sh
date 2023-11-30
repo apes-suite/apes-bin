b8796184568d4664c6f8b08df6edabce48a536e5
Harald Klimach <harald.klimach@dlr.de>
2023-11-30 15:12:04 +0100

    Include the CoCo preprocessor in waf.

--- build_waf.sh
+++ build_waf.sh
@@ -2,7 +2,8 @@ set -x
 UTESTS=$PWD/tools/utest_results.py
 SEQF=$PWD/tools/command_sequence.py
 FORD=$PWD/tools/make_fordoc.py
+COCO=$PWD/tools/coco.py,$PWD/external/coco.f90
 bindir=$PWD
 
 TOOLS=clang_compilation_database,c_bgxlc,c_nec,doxygen,eclipse,fc_nag,fc_nec,fc_nfort,fc_bgxlf,fc_cray,fc_open64,fc_pgfortran,fc_solstudio,fc_xlf,print_commands,$UTESTS,$SEQF,$FORD
-cd external/waf-2.0.26 && ./waf-light --make-waf --prelude='' --tools=$TOOLS && cp waf $bindir && cd -
+cd external/waf-2.0.26 && ./waf-light --make-waf --prelude='' --tools=$TOOLS,$COCO && cp waf $bindir && cd -
