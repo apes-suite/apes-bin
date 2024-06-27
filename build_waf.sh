25fc45da5c37c28e183fdfe4092382e2afaa19e4
Harald Klimach <harald.klimach@dlr.de>
2024-06-27 09:22:31 +0200

    Update waf to 2.1.1+flang support

--- build_waf.sh
+++ build_waf.sh
@@ -6,4 +6,4 @@ COCO=$PWD/tools/coco.py,$PWD/external/coco.f90
 bindir=$PWD
 
 TOOLS=clang_compilation_database,c_bgxlc,c_nec,doxygen,eclipse,fc_flang,fc_nag,fc_nec,fc_nfort,fc_bgxlf,fc_cray,fc_open64,fc_pgfortran,fc_solstudio,fc_xlf,print_commands,$UTESTS,$SEQF,$FORD
-cd external/waf-2.0.26 && ./waf-light --make-waf --prelude='' --tools=$TOOLS,$COCO && cp waf $bindir && cd -
+cd external/waf-2.1.1 && ./waf-light --make-waf --prelude='' --tools=$TOOLS,$COCO && cp waf $bindir && cd -
