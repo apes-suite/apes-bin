c6f5e93660f0729e464a2d57b33349712fce0882
haraldkl <harald.klimach@dlr.de>
2025-05-01 04:12:29 +0200

    Fix coco in variants (#14)
    The generation of the coco.set file during configuration, does not
    work properly with variant builds, as the coco.set file is created
    in the default build directory.
    We now generate the coco.set file during the build phase and also
    add a dependency and the necessity to create the coco.set file
    before any coco task is being run.
    Furthermore, search for the coco settings file in parent directories
    of nested wscript setups, to allow the use of one in the top most
    directory.

--- tools/coco.py
+++ tools/coco.py
@@ -6,12 +6,21 @@ import re
 import sys
 from waflib import Configure, Context, Logs, Utils
 from waflib.Task import Task
-from waflib.TaskGen import feature, extension
+from waflib.TaskGen import feature, extension, before_method
 
 def options(opt):
     opt.add_option('--coco_reports', action='store_true', default=False,
                    help='Activate output of CoCo reports')
     opt.add_option('--coco_set', action='store', help='File with coco settings')
+
+def find_in_parents(node, name):
+    '''Find the given name in the given node directory or one of its parents'''
+    np = node
+    found = np.find_resource(name)
+    while (not found) and np.parent:
+        np = np.parent
+        found = np.find_resource(name)
+    return found
 
 def configure(conf):
     conf.add_os_flags('COCOFLAGS')
@@ -33,24 +42,50 @@ def configure(conf):
     Logs.warn('Found CoCo: '+ ' '.join(conf.env.COCO))
 
     if conf.options.coco_set:
-      cocoset_src = conf.path.find_resource(conf.options.coco_set)
-      if cocoset_src:
-        cocoset_bld = conf.path.find_or_declare('coco.set')
-        cocoset_bld.write(cocoset_src.read())
-      else:
-        Logs.warn(f'Coco settings file "{conf.options.coco_set}" not found!')
+        # Using find_in_parents here to check for the definition in the top most
+        # directory if there are nested wscripts.
+        cocoset_src = find_in_parents(conf.path, conf.options.coco_set).path_from(conf.path)
+        if cocoset_src:
+            conf.env["COCOSET"] = cocoset_src
+        else:
+            Logs.warn(f'Coco settings file "{conf.options.coco_set}" not found!')
 
     if not conf.options.coco_reports:
-      # Make coco silent, if not explicitly asked for reports:
-      if conf.env.COCOFLAGS:
-        conf.env.COCOFLAGS.insert(0, '-s')
-        conf.env.COCOFLAGS.append('-ad')
-      else:
-        conf.env.COCOFLAGS = ['-s', '-ad']
+        # Make coco silent, if not explicitly asked for reports:
+        if conf.env.COCOFLAGS:
+            conf.env.COCOFLAGS.insert(0, '-s')
+            conf.env.COCOFLAGS.append('-ad')
+        else:
+            conf.env.COCOFLAGS = ['-s', '-ad']
+
+def build(bld):
+    '''Need to copy the coco.set file if requested, before any coco task starts.'''
+    cocoset = None
+    if bld.env.COCOSET:
+        cocoset = bld.env.COCOSET
+    if bld.options.coco_set:
+        cocoset = find_in_parents(bld.path, bld.options.coco_set).path_from(bld.path)
+    if cocoset:
+        topnode = bld.root.find_node(bld.top_dir)
+        bld(rule = "cp ${SRC} ${TGT}",
+            name = 'bld_cocoset',
+            source = bld.path.find_resource(cocoset),
+            target = topnode.find_or_declare('coco.set'),
+            before = 'coco')
 
 @feature('coco')
-def dummy(self):
-    pass
+@before_method('process_source', 'process_rule')
+def add_cocoset_dep(self):
+    '''Ensure that the task to copy the coco.set file is posted!
+
+       This is needed for partial builds, where the coco.set target
+       may not be included.
+       It's still needed for the coco tasks, though and has to be
+       posted if any coco task is to be built.
+    '''
+    if self.env.COCOSET:
+        ccc_set = self.bld.get_tgen_by_name('bld_cocoset')
+        ccc_set.post()
 
 class coco(Task):
     run_str = '${COCO} ${COCOFLAGS} ${TGT} ${SRC}'
@@ -61,8 +96,6 @@ class coco(Task):
     def scan(self):
         if Logs.verbose:
             Logs.debug('deps: dependencies for %r' % self.inputs)
-        # print("Looking for dependencies of:")
-        # print(self.inputs[0])
 
         INC_REGEX = r"""^\?\?\s*INCLUDE\s+(?:\w+_)?[<"'](.+?)(?=["'>])"""
         re_inc = re.compile(INC_REGEX, re.I)
@@ -92,6 +125,8 @@ class coco(Task):
             inc_node.sig = inc_node.cache_sig = Utils.h_file(inc_node.abspath())
             dnodes.append(inc_node)
             y = y+1
+        if self.env.COCOSET:
+            deplist.append(bld_dir.find_node('coco.set'))
         return(deplist, names)
 
 @extension('.fpp')
@@ -117,13 +152,7 @@ def iterativecheck(node,re_inc,incs,names,seen):
             if x in seen:
                 continue
             seen.append(x)
-            np = node.parent
-            found = np.find_resource(x)
-            # search for depend coco file recursively
-            while not found:
-                found=np.parent.find_resource(x)
-                np=np.parent
-            # print(found)
+            found = find_in_parents(node.parent, x)
             names.append(found)
             iterativecheck(found,re_inc,incs,names,seen)
     return names
